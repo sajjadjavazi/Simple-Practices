2ee6a7a29bf8495e3e248453aada1a96d8cec0f3
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-09 12:44:10 +0330

    feat(app): useState count created

--- React-Typescript-Practices/src/App.tsx
+++ React-Typescript-Practices/src/App.tsx
@@ -1,10 +1,28 @@
-
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleCount() {
+    setCount(count + 1);
+  }
   return (
-    <div>
+    <>
+      <div className="flex items-center justify-center">
+        <h1 className="bg-green-400 py-3 px-10 m-2 rounded-lg">useState</h1>
+      </div>
+      <div className="m-4 flex items-center">
+        <button
+          className="bg-slate-500 p-5 mr-3 text-white rounded-lg"
+          onClick={handleCount}
+        >
+          Increment
+        </button>
+        <p className="bg-amber-400 p-5 rounded-lg">Count: {count}</p>
+      </div>
+      <hr />
       
-    </div>
+    </>
   );
 }
 
