29d6c24cf067b4a283bbf93f38de476859b46278
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-09 15:38:32 +0330

    feat(app): mini Todo app created

--- React-Typescript-Practices/src/App.tsx
+++ React-Typescript-Practices/src/App.tsx
@@ -2,9 +2,16 @@ import { SetStateAction, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+
   const [text, setText] = useState("");
+
   const [visible, setVisible] = useState(false);
+
   const [textArea, setTextArea] = useState("");
+
+  const [todos, setTodos] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState("");
+
 
   function handleCount() {
     setCount(count + 1);
@@ -18,10 +25,22 @@ function App() {
     setVisible(!visible);
   }
 
-  const handleTextArea = (e: { target: { value: SetStateAction<string>; }; }) => {
+  const handleTextArea = (e: { target: { value: SetStateAction<string> } }) => {
     setTextArea(e.target.value);
   };
 
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
+  function handleSubmit() {
+    if (inputValue.trim()) {
+      setTodos([...todos, inputValue.trim()]);
+      setInputValue("");
+    }
+  }
+  function handleDelete(index: string | number) {
+    setTodos(todos.filter((_, i) => i !== index));
+  }
   return (
     <>
       <div className="flex items-center justify-center">
@@ -70,6 +89,35 @@ function App() {
         <p className="bg-amber-400 py-2 px-3 rounded-lg">
           Character count: {textArea.length}
         </p>
+      </div>
+      <hr />
+      <div className="flex items-center m-4">
+        <input
+          className="mr-3 ml-1 rounded-lg p-2 bg-slate-100"
+          type="text"
+          placeholder="write your todo"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button
+          className="bg-slate-500 py-2 px-3 mr-10 text-white rounded-lg"
+          onClick={handleSubmit}
+        >
+          Add
+        </button>
+        <ul className="flex">
+          {todos.map((todo, index) => (
+            <li key={index} className="bg-slate-100 mr-6 px-3 py-2 rounded-lg ">
+              {todo}
+              <button
+                className="bg-red-500 py-1 px-2 ml-2 rounded-md"
+                onClick={() => handleDelete(index)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
